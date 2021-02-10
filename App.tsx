90395f8fbdfff87ebb79b3d393a21d4702011f15
Ralvarez <R.alvgar@hotmail.com>
2021-02-10 15:18:51 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import io from "socket.io-client";
+import { Avatar } from "react-native-elements";
 
 export default function App() {
   const [recivedSocket, setRecivedSocket] = useState<String>();
@@ -14,8 +15,13 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Avatar
+        rounded
+        source={{
+          uri: "https://source.unsplash.com/user/9IZIP5KEt3E/128x90",
+        }}
+      />
       <Text>Text recived: {recivedSocket}</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
